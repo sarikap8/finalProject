6413eec6c65b62ba2bc17b1a01e416d71785ba71
1861657 <1861657@20.8.20.52>
2019-12-16 11:22:57 -0800

    Fixed randomization to have a basic arraylist knowledge question. Hope top

--- finalProject/src/finalProject/ArrayListRandom.java
+++ finalProject/src/finalProject/ArrayListRandom.java
@@ -16,46 +16,48 @@ public class ArrayListRandom extends Question {
 		return q.getCounterMsg();
 	}
 
-	public String[] switchAnswers(String optionA, String optionB) {
+	/*public String[] switchAnswers(String optionA, String optionB) {
 		String temp = optionA;
 		optionA = optionB;
 		optionB = temp;
 		String[] ew = { optionA, optionB };
 		return ew;
-	}
+	}*/
 
 	@Override
 	protected void setupQuestionData() {
         ConsoleMethods.println("ArrayList class setupQuestionData method");
-
 		Random rand = new Random();
 		ArrayList<Integer> randomArrayList = new ArrayList(); 
 		for (int i = 0; i < rand.nextInt(); i++) {
 			int ugh = rand.nextInt(); 
 			randomArrayList.add(ugh);  
 		} 
-		int test = rand.nextInt(2);
-		this.question = "Which of the following is the correct way to remove the value " + test
-				+ " from the list below? \n nums = [0,1,2,3,4,5,6]";
-		//int switchcase = rand.nextInt(4);
-		// format question
-			/*this.question = "Which of the following is the correct way to remove the value " + test
-					+ " from the list below? \n nums = [0,1,2,3,4,5,6]";
-			this.choiceA = "nums.remove(5);";
-			this.choiceB = "nums.remove(0);";
-			this.choiceC = "nums.remove(1);";
-			this.choiceD = "nums.remove(2);";
-			this.choiceE = "None of the above";
-			if (test == 0) {
-				answer = this.choiceB;
-			}
-			if (test == 1) {
-				answer = this.choiceC;
-			}
-			if (test == 2) {
-				answer = this.choiceD;
+		int selectionIndex = rand.nextInt();
+		if (selectionIndex > 0) {
+		this.question = "Which of the following is the correct way to remove the value at location " + selectionIndex + " from an ArrayList below?"; 
+		this.choiceA = "nums.remove(" + selectionIndex + ")";
+		this.choiceB = "nums.remove(1);";
+		this.choiceC = "nums.remove(2);";
+		this.choiceD = "nums.remove(3);";
+		this.choiceE = "We don't know until we see the arraylist";
 
-			}*/
+			answer = this.choiceE;
+			this.answerKey = this.charE; 
+		}
+		if (selectionIndex < 0) {
+		this.question = "Which of the following is the correct way to add the value at location " + selectionIndex + " from an ArrayList below?"; 
+		this.choiceA = "nums.add(" + selectionIndex + ")";
+		this.choiceB = "nums.add(1);";
+		this.choiceC = "nums.add(2);";
+		this.choiceD = "nums.add(3);";
+		this.choiceE = "We don't know until we see the arraylist";
+
+			answer = this.choiceE;
+			this.answerKey = this.charE; 
+		}
+
+
 	}
 }
 
